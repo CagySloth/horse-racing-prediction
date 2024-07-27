b4cc56644115be9447eac7aafbef93e2b2922fef
reagan <reagan.chan@ncs.co>
2024-07-27 23:42:16 +0800

    restructure repo 3

--- simple.py
+++
@@ -1,8 +0,0 @@
-from scrapers.scrape_race import scrape_race
-
-assert scrape_race()
-
-# df = scrape_horse("racing/information/Chinese/Horse/Horse.aspx?HorseId=HK_2021_G463")
-
-# print("df:", df)
-# df.to_csv("temp2.csv")
